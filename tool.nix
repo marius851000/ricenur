156617308c2adaed58ff82e100bddfacb3f6dfcf
marius david <mariusdavid@laposte.net>
2022-04-11 09:23:01 +0200

    fix for panon not working on unstable

--- tool.nix
+++ tool.nix
@@ -1,9 +1,9 @@
 {
   make_plasma_workspace_overlay = patch: (
     self: super: {
-      plasma5Packages = super.plasma5Packages // {
-        plasma5 = super.plasma5Packages.plasma5 // {
-          plasma-workspace = super.plasma5Packages.plasma5.plasma-workspace.overrideAttrs (old: patch self old);
+      libsForQt5 = super.libsForQt5 // {
+        plasma5 = super.libsForQt5.plasma5 // {
+          plasma-workspace = super.libsForQt5.plasma5.plasma-workspace.overrideAttrs (old: patch self old);
         };
       };
     }
